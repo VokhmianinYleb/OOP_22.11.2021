ad6023c3867b8dbd0dbac224a13415aee7ffae23
Yleb22 <80745115+Yleb22@users.noreply.github.com>
2021-11-23 11:59:36 +0200

    Create Counts.h

---
+++ lb19/Example_5/Counts.h
@@ -0,0 +1,19 @@
+// оголошується 3 класи зі статичними змінними
+// клас, що містить статичну змінну count
+class CCount1
+{
+public:
+	static int count1; // статичний член даних, ключове слово static
+};
+
+class CCount2
+{
+public:
+	static int count2; // статичний член даних, ключове слово static
+};
+
+class CCount3
+{
+public:
+	static int count3; // статичний член даних, ключове слово static
+};
